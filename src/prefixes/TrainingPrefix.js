f96ab794603dba108f84f45bc0621aa2f579e4fa
arinkhatua <arin.khatua@simpleviewinc.com>
2024-05-06 15:15:03 -0700

    Fix code style for eslint compliance

--- src/prefixes/TrainingPrefix.js
+++ src/prefixes/TrainingPrefix.js
@@ -2,15 +2,15 @@ const Movies = require("./collections/Movies");
 const People = require("./collections/People");
 
 class TrainingPrefix {
-    constructor({ graphUrl, graphServer }) {
-        this.name = "training";
+	constructor({ graphUrl, graphServer }) {
+		this.name = "training";
 
-        this._graphUrl = graphUrl;
-        this._graphServer = graphServer;
+		this._graphUrl = graphUrl;
+		this._graphServer = graphServer;
 
-        this.movies = new Movies({ graphUrl, graphServer });
-        this.people = new People({ graphUrl, graphServer });
-    }
+		this.movies = new Movies({ graphUrl, graphServer });
+		this.people = new People({ graphUrl, graphServer });
+	}
 }
 
 module.exports = TrainingPrefix;
